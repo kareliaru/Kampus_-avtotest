3ba4b1506fa04df3d8e727a36bb40ffc9826357f
kareliaru <156843184+kareliaru@users.noreply.github.com>
2024-08-08 19:47:42 +0300

    Add files via upload

---
+++ 2Code.js
@@ -0,0 +1,5 @@
+let count = 1;
+console.log( 2 * count );// 2
+console.log(count++);//увеличивает на 1, но возвращает старое значение 1
+console.log(++count)//возвращает 3
+console.log( 2 * count ); // 6
