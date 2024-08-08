4e63c45cc9cb8d0138172d126f11afc1b4121b99
kareliaru <156843184+kareliaru@users.noreply.github.com>
2024-08-08 18:56:33 +0300

    Update firstCode.js

--- firstCode.js
+++ firstCode.js
@@ -1,2 +1,2 @@
-let firstCode = 'Hi everybody! I write Java Script code. It`s cool!' 
+let firstCode = 'Hi everybody! I write Java Script code. It`s cool! Don`t laugh at me, this is the first time for me' 
 console.log(firstCode)
