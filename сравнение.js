fe2e38b5528528cc410f011301e23c23c742d20c
kareliaru <156843184+kareliaru@users.noreply.github.com>
2024-08-13 17:49:03 +0300

    Add files via upload

---
+++ сравнение.js
@@ -0,0 +1,14 @@
+console.log(5 == 4) //false
+console.log(2 < 3)  //true
+console.log(4>3)  //true
+
+let result = 5 > 4
+console.log (result) //true
+
+console.log(5 > 4) //true
+console.log("ананас" > "яблоко") //false
+console.log("2" > "12") //true
+console.log(undefined == null) //true
+console.log(undefined === null) //false
+console.log(null == "\n0\n") //false
+console.log(null === +"\n0\n") //false
