3de5765a9b688611c4a56d1db968062960b999f6
kareliaru <156843184+kareliaru@users.noreply.github.com>
2024-09-08 09:19:24 +0300

    Create (Задача 2.13.5 Повторять цикл, пока ввод неверен).js

---
+++ (Задача 2.13.5 Повторять цикл, пока ввод неверен).js
@@ -0,0 +1,10 @@
+cicle:
+while (true) {
+
+    let value = +prompt("Введите число", '');
+    if (value > 100)
+        alert(`Ваше число: ${value}`);
+    if (value < 100)
+        alert ("Введите число > 100");
+    if (!value) break cicle; 
+  }
