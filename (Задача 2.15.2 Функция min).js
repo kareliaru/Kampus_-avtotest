0367ed3cbbb75b6bed4eaf3a730a9b6a33d8f230
kareliaru <156843184+kareliaru@users.noreply.github.com>
2024-09-08 13:42:48 +0300

    Create (Задача 2.15.2 Функция min).js

---
+++ (Задача 2.15.2 Функция min).js
@@ -0,0 +1,10 @@
+// Напишите функцию min(a,b), которая возвращает меньшее из чисел a и b.
+
+function min(a, b){
+    if (a < b)
+       return a;
+    else if (a > b)
+        return b;
+    else
+        return a
+}
