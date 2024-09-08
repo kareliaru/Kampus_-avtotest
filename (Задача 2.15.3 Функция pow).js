5b7ac2b2c5c12b8346033e454c729775296db813
kareliaru <156843184+kareliaru@users.noreply.github.com>
2024-09-08 14:22:51 +0300

    Update (Задача 2.15.3 Функция pow).js

--- (Задача 2.15.3 Функция pow).js
+++ (Задача 2.15.3 Функция pow).js
@@ -1,5 +1,7 @@
 // Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
-
+let x = +prompt("x?", '');
+let n = +prompt("n?", '');
 function pow(x, n){
     return(x ** n)
 }
+alert( pow(x, n) );
